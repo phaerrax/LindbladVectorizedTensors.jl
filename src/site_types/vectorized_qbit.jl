ddf38899620ee98ffac6da96a9b63d8404cbba66
Davide Ferracin <davide.ferracin@protonmail.com>
2024-06-11 19:02:08 +0300

    Fix ordering in PTM product basis
    The multi-qbit PTM basis was defined in an order that wasn't compatible
    with how ITensor decomposes the matrices when constructing a multi-site
    operator with the `op` function; the Index arguments needed to be
    written in reverse order when calling `op` to obtain a correct result.

--- src/site_types/vectorized_qbit.jl
+++ src/site_types/vectorized_qbit.jl
@@ -74,20 +74,36 @@ ITensors.space(::SiteType"vQubit") = 4
 
 # "Pauli transfer matrix" basis
 # -----------------------------
-function ptmbasis(n_qbits::Int)
+function ptmbasis(nqbits::Int)
     st = SiteType("Qubit")
     σ =
         (1 / sqrt(2)) .*
         [Matrix(I, 2, 2), ITensors.op("X", st), ITensors.op("Y", st), ITensors.op("Z", st)]
 
-    if n_qbits == 1
+    if nqbits == 1
         return σ  # and don't bother with the rest
     else
         # σxn = Base.product(s, s, s... [n times])
-        σxn = Base.product(repeat([σ], n_qbits)...)
+        σxn = Base.product(repeat([σ], nqbits)...)
         # Each element of σxn is a list (s_1, s_2, ..., s_n) where s_i is a Pauli matrix.
+        # For example, for two bits, if `tp(a, b) = "$a ⊗ $b" then
+        #   S = ["s$n" for n in 0:3];
+        #   [tp(s...) for s in Base.product(repeat([S], 2)...)] -->
+        #    "s0 ⊗ s0"  "s0 ⊗ s1"  "s0 ⊗ s2"  "s0 ⊗ s3"
+        #    "s1 ⊗ s0"  "s1 ⊗ s1"  "s1 ⊗ s2"  "s1 ⊗ s3"
+        #    "s2 ⊗ s0"  "s2 ⊗ s1"  "s2 ⊗ s2"  "s2 ⊗ s3"
+        #    "s3 ⊗ s0"  "s3 ⊗ s1"  "s3 ⊗ s2"  "s3 ⊗ s3"
+
+        # By calling `Base.vec` on this matrix we stack its columns, but we want to unroll
+        # by rows instead, so we transpose it first. The call to `permutedims` below
+        # does this transposition "on all dimensions".
+        # FIXME Why do we need rows instead of columns here?
+        tensorproducts = [kron(s...) for s in σxn]
+        perm = reverse(ntuple(i -> i, Val{nqbits}()))  # (nqbits, nqbits - 1, ..., 2, 1)
+        tensorproducts_transposed = permutedims(tensorproducts, perm)
+
         # We transform each of them in the tensor product s_1 ⊗ s_2 ⊗ ... ⊗ s_n.
-        return Base.vec([kron(s...) for s in σxn])
+        return Base.vec(tensorproducts_transposed)
     end
 end
 
