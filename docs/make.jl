c865c28e222d6e6d31e9e7d02e6aededd1251c9d
Davide Ferracin <davide.ferracin@protonmail.com>
2026-04-02 09:55:32 +0200

    fix: reorganise `test` and `docs` Julia projects
    Add missing dependencies and settings to the documentation and testing
    subprojects.

--- docs/make.jl
+++ docs/make.jl
@@ -1,35 +1,73 @@
-using LindbladVectorizedTensors
-using ITensors
-using ITensorMPS
 using Documenter
+using LindbladVectorizedTensors
 
-DocMeta.setdocmeta!(
-    LindbladVectorizedTensors,
-    :DocTestSetup,
-    :(using LindbladVectorizedTensors);
-    recursive=true,
-)
+# doctest dependencies
+using ITensors, ITensorMPS
 
 makedocs(;
     modules=[LindbladVectorizedTensors],
+    sitename="LindbladVectorizedTensors.jl",
+    repo=Remotes.GitHub("phaerrax", "LindbladVectorizedTensors.jl"),
     checkdocs=:exported,
     authors="Davide Ferracin <davide.ferracin@protonmail.com> and contributors",
-    sitename="LindbladVectorizedTensors.jl",
-    format=Documenter.HTML(;
-        canonical="https://phaerrax.github.io/LindbladVectorizedTensors.jl",
-        edit_link="main",
-        assets=String[],
-    ),
     pages=[
         "Home" => "index.md",
         "New site types" => "included_site_types.md",
         "Examples" => "gksl_example.md",
         "Reference" => "physical_operators.md",
     ],
+    format=Documenter.HTML(;
+        mathengine=Documenter.MathJax3(
+            Dict(
+                :tex => Dict(
+                    :inlineMath => [["\$", "\$"], [raw"\(", raw"\)"]],
+                    :tags => "ams",
+                    :packages =>
+                        ["base", "ams", "autoload", "configmacros", "mathtools"],
+                    :macros => Dict(
+                        :C => [raw"\mathbb{C}"],
+                        :Im => [raw"\mathrm{Im}"],
+                        :N => [raw"\mathbb{N}"],
+                        :Num => [raw"\mathscr{N}"],
+                        :R => [raw"\mathbb{R}"],
+                        :Re => [raw"\mathrm{Re}"],
+                        :abs => [raw"\lvert #1 \rvert", 1],
+                        :adj => [raw"#1^\dagger", 1],
+                        :avg => [raw"\langle #1\rangle", 1],
+                        :blank => [raw"{-}"],
+                        :bra => [raw"\langle #1 \rvert", 1],
+                        :conj => [raw"\bar{#1}", 1],
+                        :defeq => [raw"\mathrel{\mathop:}="],
+                        :diag => [raw"\operatorname{diag}"],
+                        :dissipator => [raw"\mathscr{D}"],
+                        :fcomm => [raw"\{#1\}", 1],
+                        :id => [raw"1"],
+                        :imat => [raw"I_{#1}", 1, ""],
+                        :innp => [raw"\langle #1, #2\rangle", 2],
+                        :iu => [raw"\mathrm{i}"],
+                        :ket => [raw"\lvert #1 \rangle", 1],
+                        :lindblad => [raw"\mathscr{L}"],
+                        :norm => [raw"\lVert #1 \rVert", 1],
+                        :outp => [raw"\lvert #1\rangle \langle #2\rvert", 2],
+                        :paulim => [raw"\sigma_-^{(#1)}", 1, ""],
+                        :paulip => [raw"\sigma_+^{(#1)}", 1, ""],
+                        :paulix => [raw"\sigma_x^{(#1)}", 1, ""],
+                        :pauliy => [raw"\sigma_y^{(#1)}", 1, ""],
+                        :pauliz => [raw"\sigma_z^{(#1)}", 1, ""],
+                        :phantomadj => [raw"^{\vphantom{\dagger}}"],
+                        :sb => [raw"_{#1}", 1],
+                        :set => [raw"\{\, #1 \;\vert\; #2\,\}", 2],
+                        :spinup => [raw"\uparrow"],
+                        :spindown => [raw"\downarrow"],
+                        :tr => [raw"\operatorname{tr}"],
+                        :transpose => [raw"#1^{\mathrm{T}}", 1],
+                        :tsp => [raw"^{\otimes #1}", 1],
+                    ),
+                ),
+            ),
+        ),
+    ),
 )
 
-deploydocs(;
-    branch="gh-pages",
-    repo="github.com/phaerrax/LindbladVectorizedTensors.jl",
-    devbranch="main",
-)
+# Automatically deploy documentation to gh-pages.
+deploydocs(; repo="github.com/phaerrax/LindbladVectorizedTensors.jl")
